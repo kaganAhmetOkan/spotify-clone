e1b5446e69cec669da3c8058d6d208ee1fb5014a
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2023-12-26 02:12:37 +0300

    Created add playlist button

--- components/LibraryButton/LibraryButton.jsx
+++ components/LibraryButton/LibraryButton.jsx
@@ -5,16 +5,22 @@ import Image from "next/image";
 export default function LibraryButton({ iconSize, minimized, setMinimized }) {
   const libraryIcon = minimized ? "/icons/library.png" : "/icons/library_filled.png";
   const buttonTitle = minimized ? "Expand Your Library" : "Collapse Your Library";
+  const smallIconSize = 16;
 
   function toggleMinimized() {
     setMinimized(!minimized);
   };
 
   return (
-    <button className={style.main} title={buttonTitle} onClick={toggleMinimized}>
-        <Image src={libraryIcon} alt="library" width={iconSize} height={iconSize} />
-        <span>Library</span>
-    </button>
+    <div className={style.main}>
+      <button className={style.title} title={buttonTitle} onClick={toggleMinimized}>
+          <Image src={libraryIcon} alt="library" width={iconSize} height={iconSize} />
+          <span>Library</span>
+      </button>
+      <button className={style.button} title="Create playlist or folder">
+        <Image src={`/icons/plus.png`} alt="new playlist" width={smallIconSize} height={smallIconSize} />
+      </button>
+    </div>
   );
 };
 
