ab8e2497dfff515584c73c5a19f78102973526eb
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2024-01-01 13:09:00 +0300

    Created NowPlaying component

--- components/Controller/Controller.tsx
+++ components/Controller/Controller.tsx
@@ -1,9 +1,10 @@
 import style from "./Controller.module.scss";
+import NowPlaying from "../NowPlaying/NowPlaying";
 
 export default function Controller() {
   return (
     <div className={style.main}>
-      Controller
+      <NowPlaying />
     </div>
   );
 };
---
+++ components/NowPlaying/NowPlaying.tsx
@@ -0,0 +1,9 @@
+import style from "./NowPlaying.module.scss";
+
+export default function NowPlaying() {
+  return (
+    <div className={style.main}>
+      NowPlaying
+    </div>
+  );
+};
