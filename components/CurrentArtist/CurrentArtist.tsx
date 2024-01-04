7a612c51bbcc823b56605d3e5ff3b58645505348
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2024-01-04 11:18:53 +0300

    Added artist image

--- components/CurrentArtist/CurrentArtist.tsx
+++ components/CurrentArtist/CurrentArtist.tsx
@@ -1,4 +1,5 @@
 import style from "./CurrentArtist.module.scss";
+import Image from "next/image";
 import FollowButton from "../FollowButton/FollowButton";
 
 export default function CurrentArtist() {
@@ -10,9 +11,16 @@ export default function CurrentArtist() {
   
   return (
     <div className={style.main}>
-      <h2>About the artist</h2>
+      <div className={style.thumbnail}>
+        <h2>About the artist</h2>
+        <Image
+          src={"/images/artist.jpg"}
+          alt="artist"
+          fill
+        />
+      </div>
       <h1>{artist.name}</h1>
-      <div>
+      <div className={style.details}>
         <span>{artist.listeners}</span>
         <FollowButton />
       </div>
@@ -20,3 +28,5 @@ export default function CurrentArtist() {
     </div>
   );
 };
+
+// TODO: Artist image is placeholder
