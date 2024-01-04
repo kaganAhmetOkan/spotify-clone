8a1a983b048fbfefc55042ba7693dae310d5e1c8
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2024-01-04 11:39:35 +0300

    Added toggle About

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,11 +1,16 @@
+"use client";
 import style from "./About.module.scss";
 import CurrentTrack from "../CurrentTrack/CurrentTrack";
 import CurrentArtist from "../CurrentArtist/CurrentArtist";
 import NextTrack from "../NextTrack/NextTrack";
+import { useAtomValue } from "jotai";
+import { aboutAtom } from "@/store";
 
 export default function About() {
+  const about = useAtomValue(aboutAtom);
+  
   return (
-    <div className={style.main}>
+    <div className={style.main} data-visible={about}>
       <CurrentTrack />
       <CurrentArtist />
       <NextTrack />
