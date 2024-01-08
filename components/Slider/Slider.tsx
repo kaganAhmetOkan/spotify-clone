d0a1514fc0ba61f81c1dd765d9ed7a9d766dbed9
Kağan Ahmet Okan <okaganahmet@gmail.com>
2024-01-08 20:13:14 +0300

    Added rc-slider for consistent slider style across browsers

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -1,5 +1,7 @@
 "use client";
 import style from "./Slider.module.scss";
+import CustomSlider from "rc-slider";
+import "rc-slider/assets/index.css";
 import { useAtom } from "jotai";
 import { currentTrackAtom } from "@/store";
 import getMinutes from "@/lib/getMinutes";
@@ -8,22 +10,22 @@ export default function Slider() {
   const [currentTrack, setCurrentTrack] = useAtom(currentTrackAtom);
   const { progress, duration } = currentTrack;
 
-  function setProgress(event: React.ChangeEvent<HTMLInputElement>) {
-    const value = +event.currentTarget.value;
-
+  function setProgress(value: number | number[]) {
+    if (typeof value !== "number") return;
     setCurrentTrack({ ...currentTrack, progress: value });
   };
 
   return (
     <div className={style.main}>
       <span>{getMinutes(progress)}</span>
-      <input
-        type="range"
+      <CustomSlider
+        className={style.input}
+        classNames={{ track: style.track, handle: style.handle, rail: style.rail }}
         min={0}
         max={duration}
         value={progress}
         onChange={setProgress}
-      ></input>
+      />
       <span>{getMinutes(duration)}</span>
     </div>
   );
